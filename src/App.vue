4d2547f9cd9f3c92701a567763b9daca68300cc8
dimar55 <dimargalvan55@gmail.com>
2021-11-28 08:45:09 -0500

    update

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,11 @@
 <template>
   <div id="app" class="app">
-    <div class="header">
-      <ul class="datosHeader">
-        <div class="cLi">
-          <li><img src="./assets/iconLocation.png" alt="" /></li>
-          <li>Calle 2 #9-69 Barrio Santander / Villa del Rosario</li>
-        </div>
-        <div class="cLi">
-          <li><img src="./assets/iconPhone.png" alt="" /></li>
-          <li>3156252089</li>
-        </div>
-      </ul>
-    </div>
+    <router-view></router-view>
   </div>
- <Descripcion></Descripcion>
-  <footer class="piePagina">
-    <div class="info">
-      <h2>INFORMACIÓN</h2>
-      <ul class="pList">
-        <div class="pLocation">
-          <li><img src="./assets/iconLocation.png" alt="" /></li>
-          <li>Calle 2 #9-69 Barrio Santander / Villa del Rosario</li>
-        </div>
-        <div class="pPhone">
-          <li><img src="./assets/iconPhone.png" alt="" /></li>
-          <li>3156252089</li>
-        </div>
-      </ul>
-    </div>
-    <div class="fcategorias">
-      <h2>CATEGORÍAS</h2>
-      <p>Dama / Caballero / Niños</p>
-    </div>
-  </footer>
 </template>
 <script>
-import Navi from "./components/barraNav.vue";
-import Welcome from "./components/bienvenido.vue";
-import SingUp from "./components/singUp.vue";
-import RegistrarOperador from "./components/registrarOperador.vue";
-import RegistrarProveedor from "./components/registrarProveedor.vue";
-import Header from "./components/header.vue";
-import Descripcion from "./components/descripcion.vue";
 export default {
-  name: "App",
-  components: { Navi, Welcome, SingUp, RegistrarOperador, RegistrarProveedor, Header, Descripcion },
+  name: "App"
 };
 </script>
 
