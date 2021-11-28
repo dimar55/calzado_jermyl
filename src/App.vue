aa96352ad857a3f649abaa56c65d9f40f83e52a9
David Celis <davidcelis682@gmail.com>
2021-11-28 08:23:55 -0500

    Update

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,7 @@
 <template>
   <div id="app" class="app">
-    <div class="header">
-      <ul class="datosHeader">
-        <div class="cLi">
-          <li><img src="./assets/iconLocation.png" alt="" /></li>
-          <li>Calle 2 #9-69 Barrio Santander / Villa del Rosario</li>
-        </div>
-        <div class="cLi">
-          <li><img src="./assets/iconPhone.png" alt="" /></li>
-          <li>3156252089</li>
-        </div>
-      </ul>
-    </div>
+    <router-view></router-view>
   </div>
- <Descripcion></Descripcion>
-  <footer class="piePagina">
-    <div class="info">
-      <h2>INFORMACIÓN</h2>
-      <ul class="pList">
-        <div class="pLocation">
-          <li><img src="./assets/iconLocation.png" alt="" /></li>
-          <li>Calle 2 #9-69 Barrio Santander / Villa del Rosario</li>
-        </div>
-        <div class="pPhone">
-          <li><img src="./assets/iconPhone.png" alt="" /></li>
-          <li>3156252089</li>
-        </div>
-      </ul>
-    </div>
-    <div class="fcategorias">
-      <h2>CATEGORÍAS</h2>
-      <p>Dama / Caballero / Niños</p>
-    </div>
-  </footer>
 </template>
 <script>
 import Navi from "./components/barraNav.vue";
